b2f5a1e3faf022740f072d6a20eb8738888a03b4
Radif69 <132708368+Radif69@users.noreply.github.com>
2025-04-04 08:08:29 +0600

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-// server.js
 const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
@@ -24,16 +23,43 @@ app.get('/', (req, res) => {
   res.send('Watch Together server is running');
 });
 
+// Add a health check endpoint
+app.get('/health', (req, res) => {
+  res.json({
+    status: 'ok',
+    uptime: process.uptime(),
+    timestamp: Date.now(),
+    activeRooms: Object.keys(rooms).length
+  });
+});
+
+// Debug endpoint to view active rooms
+app.get('/rooms', (req, res) => {
+  const roomsInfo = {};
+  
+  for (const roomId in rooms) {
+    roomsInfo[roomId] = {
+      hostId: rooms[roomId].host,
+      userCount: Object.keys(rooms[roomId].users).length,
+      usernames: Object.values(rooms[roomId].users).map(u => u.username)
+    };
+  }
+  
+  res.json(roomsInfo);
+});
+
 io.on('connection', (socket) => {
   console.log('User connected:', socket.id);
   
+  // Handle joining a room
   socket.on('joinRoom', (data) => {
     const { roomId, username, isHost } = data;
+    console.log(`${username} (${socket.id}) is joining room ${roomId} as ${isHost ? 'host' : 'guest'}`);
     
-    // Join the room
+    // Join the socket to the room
     socket.join(roomId);
     
-    // Store user data
+    // Initialize room if it doesn't exist
     if (!rooms[roomId]) {
       rooms[roomId] = {
         host: isHost ? socket.id : null,
@@ -41,6 +67,7 @@ io.on('connection', (socket) => {
       };
     }
     
+    // Store user data
     rooms[roomId].users[socket.id] = {
       username,
       isHost
@@ -51,70 +78,95 @@ io.on('connection', (socket) => {
       rooms[roomId].host = socket.id;
     }
     
-    // Broadcast to room that user joined
-    socket.to(roomId).emit('userJoined', {
+    // Notify everyone in the room that a new user joined
+    // Important: this must use "to" not "in" to broadcast to all clients including the sender
+    io.to(roomId).emit('userJoined', {
       username,
       isHost
     });
     
     console.log(`${username} joined room ${roomId}`);
+    console.log(`Room ${roomId} now has ${Object.keys(rooms[roomId].users).length} users`);
   });
   
+  // Handle chat messages
   socket.on('chatMessage', (data) => {
     const { roomId, username, message } = data;
+    console.log(`Chat message in room ${roomId} from ${username}: ${message}`);
     
-    // Broadcast message to all users in room except sender
+    // Broadcast message to all OTHER users in room
     socket.to(roomId).emit('chatMessage', {
       username,
       message
     });
   });
   
+  // Handle video control events
   socket.on('videoControl', (data) => {
     const { roomId, currentTime, isPlaying } = data;
     
+    // Check if room exists
+    if (!rooms[roomId]) {
+      console.log(`Room ${roomId} not found for video control`);
+      return;
+    }
+    
     // Check if user is host (only host can control video)
-    const room = rooms[roomId];
-    if (room && room.host === socket.id) {
-      // Broadcast video control to all clients except sender
+    if (rooms[roomId].host === socket.id) {
+      console.log(`Host (${socket.id}) sending video control: time=${currentTime}, playing=${isPlaying}`);
+      
+      // Forward control to everyone else in room
       socket.to(roomId).emit('videoControl', {
         currentTime,
         isPlaying
       });
+    } else {
+      console.log(`Non-host user (${socket.id}) attempted video control`);
     }
   });
-  
+
+  // Handle disconnections
   socket.on('disconnect', () => {
-    console.log('User disconnected:', socket.id);
+    console.log(`User disconnected: ${socket.id}`);
     
-    // Find which room the user was in
+    // Find which room this user was in
+    let userRoom = null;
+    let wasHost = false;
+    
     for (const roomId in rooms) {
-      const room = rooms[roomId];
-      if (room.users[socket.id]) {
-        const username = room.users[socket.id].username;
+      if (rooms[roomId].users[socket.id]) {
+        userRoom = roomId;
+        wasHost = rooms[roomId].host === socket.id;
         
-        // Notify others that user left
+        // Store username before removing user
+        const username = rooms[roomId].users[socket.id].username;
+        
+        // Remove user from room
+        delete rooms[roomId].users[socket.id];
+        
+        // Notify room that user left
         socket.to(roomId).emit('userLeft', {
           username
         });
         
-        // Delete user from room
-        delete room.users[socket.id];
+        console.log(`${username} left room ${roomId}`);
         
-        // If user was host, assign new host or delete room if empty
-        if (room.host === socket.id) {
-          const remainingUsers = Object.keys(room.users);
-          if (remainingUsers.length > 0) {
-            // Assign first remaining user as host
-            room.host = remainingUsers[0];
-            room.users[room.host].isHost = true;
-            
-            // Notify new host
-            io.to(room.host).emit('becomeHost');
-          } else {
-            // Delete empty room
-            delete rooms[roomId];
-          }
+        // If room is empty, delete it
+        if (Object.keys(rooms[roomId].users).length === 0) {
+          console.log(`Deleting empty room ${roomId}`);
+          delete rooms[roomId];
+        } 
+        // If user was host, assign a new host
+        else if (wasHost) {
+          // Pick first remaining user as new host
+          const newHostId = Object.keys(rooms[roomId].users)[0];
+          rooms[roomId].host = newHostId;
+          rooms[roomId].users[newHostId].isHost = true;
+          
+          // Notify new host
+          io.to(newHostId).emit('becomeHost');
+          
+          console.log(`New host for room ${roomId}: ${newHostId}`);
         }
         
         break;
@@ -123,6 +175,7 @@ io.on('connection', (socket) => {
   });
 });
 
+// Start server
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
